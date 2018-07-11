671cb2be8fa56d4961fd99e38be6d14381a786f0
issa-mour <issa.mour2@gmail.com>
2018-07-11 14:37:46 +0300

    add desribe to test

--- src/components/social-card/SocialCardHeader.test.js
+++ src/components/social-card/SocialCardHeader.test.js
@@ -2,12 +2,14 @@ import React from 'react';
 import SocialCardHeader from './SocialCardHeader';
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-	const tree = renderer
-		.create(<SocialCardHeader
-			image="https://scontent.ftlv4-1.fna.fbcdn.net/v/t1.0-1/p80x80/n.jpg"
-			profileText="Vevo"
-			time="3 hrs" />)
-		.toJSON();
-	expect(tree).toMatchSnapshot();
+describe('SocialCardHeader test', () => {
+	it('renders correctly', () => {
+		const tree = renderer
+			.create(<SocialCardHeader
+				image="https://scontent.ftlv4-1.fna.fbcdn.net/v/t1.0-1/p80x80/n.jpg"
+				profileText="Vevo"
+				time="3 hrs" />)
+			.toJSON();
+		expect(tree).toMatchSnapshot();
+	});	
 });
