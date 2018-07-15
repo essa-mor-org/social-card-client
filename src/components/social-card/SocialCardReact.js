ed53a5d632bed6bbf148f9d45eb1c1172bd8491c
issa-mour <issa.mour2@gmail.com>
2018-07-15 09:50:29 +0300

    react callbacks

--- src/components/social-card/SocialCardReact.js
+++ src/components/social-card/SocialCardReact.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import ReactionButton from '../buttons/ReactionButton';
 
 import './SocialCardReact.css';
 
-function SocialCardReact(){
+function SocialCardReact({onLike, onComment, onShare}){
 	return (<div className="Social_react">
-		<ReactionButton text="Like" className="Reaction_like" />
-		<ReactionButton text="Comment" className="Reaction_comment" />
-		<ReactionButton text="Share" className="Reaction_share" />
+		<ReactionButton text="Like" className="Reaction_like" onClick={onLike} />
+		<ReactionButton text="Comment" className="Reaction_comment" onClick={onComment}/>
+		<ReactionButton text="Share" className="Reaction_share" onClick={onShare}/>
 	</div>);
 }
+
+SocialCardReact.propTypes = {
+	onLike: PropTypes.func.isRequired,
+	onComment: PropTypes.func.isRequired,
+	onShare: PropTypes.func.isRequired
+};
 
 export default SocialCardReact;
--- src/components/social-card/SocialCardReact.test.js
+++ src/components/social-card/SocialCardReact.test.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import SocialCardReact from './SocialCardReact';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 describe('SocialCardReact test', () => {
 	it('renders correctly', () => {
 		const tree = renderer
-			.create(<SocialCardReact />)
+			.create(<SocialCardReact onLike={jest.fn()} onComment={jest.fn()} onShare={jest.fn()}/>)
 			.toJSON();
 		expect(tree).toMatchSnapshot();
+	});
+
+	it('renders correctly - click callback', () => {
+		const likeCallBack = jest.fn();
+		const commentCallBack = jest.fn();
+		const shareCallBack = jest.fn();
+		const wrapper = shallow(<SocialCardReact 
+			onLike={likeCallBack} 
+			onComment={commentCallBack}
+			onShare={shareCallBack}/>);
+		wrapper.find('.Reaction_like').simulate('click');
+		expect(likeCallBack).toHaveBeenCalledTimes(1);
+		wrapper.find('.Reaction_comment').simulate('click');
+		expect(commentCallBack).toHaveBeenCalledTimes(1);
+		wrapper.find('.Reaction_share').simulate('click');
+		expect(shareCallBack).toHaveBeenCalledTimes(1);
 	});
 });
