77fa2374fae8a4dd52f62ee2f157a4df657bd59c
issa-mour <issa.mour2@gmail.com>
2018-07-15 10:32:05 +0300

    add comment call back

--- src/components/social-card/comments/SocialCardComments.test.js
+++ src/components/social-card/comments/SocialCardComments.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SocialCardComments from './SocialCardComments';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 describe('SocialCardComments test', () => {
 	it('renders correctly - no comments', () => {
 		const tree = renderer
 			.create(<SocialCardComments
+				onComment={jest.fn()}
 				profileImage="https://scontent.ftlv4-1.fna.fbcdn.net/v/t1.0-1/p80x80/n.jpg" />)
 			.toJSON();
 		expect(tree).toMatchSnapshot();
@@ -14,14 +16,33 @@ describe('SocialCardComments test', () => {
 	it('renders correctly - with comments', () => {
 		const tree = renderer
 			.create(<SocialCardComments
+				onComment={jest.fn()}
 				profileImage="https://scontent.ftlv4-1.fna.fbcdn.net/v/t1.0-1/p80x80/n.jpg"
 				comments={[{
-					id:1,
+					id: 1,
 					profileImage: 'https://scontent.ftlv4-1.fna.fbcdn.net/v/t1.0-1/p80x80/n.jpg',
 					profileName: 'Vevo',
 					comment: 'Hi'
 				}]} />)
 			.toJSON();
 		expect(tree).toMatchSnapshot();
+	});
+
+	it('renders correctly - add comment and press enter', () => {
+		const commentCallBack = jest.fn();
+		const wrapper = shallow(<SocialCardComments
+			onComment={commentCallBack}
+			profileImage="https://scontent.ftlv4-1.fna.fbcdn.net/v/t1.0-1/p80x80/n.jpg" />);
+		wrapper.find('.Social_comments_input').simulate('keyPress',{key: 'Enter', target:{value:'abc'}});
+		expect(commentCallBack).toHaveBeenCalledWith('abc');
+	});
+
+	it('renders correctly - add comment do not press enter', () => {
+		const commentCallBack = jest.fn();
+		const wrapper = shallow(<SocialCardComments
+			onComment={commentCallBack}
+			profileImage="https://scontent.ftlv4-1.fna.fbcdn.net/v/t1.0-1/p80x80/n.jpg" />);
+		wrapper.find('.Social_comments_input').simulate('keyPress',{key: '', target:{value:'abc'}});
+		expect(commentCallBack).toHaveBeenCalledTimes(0);
 	});
 });
