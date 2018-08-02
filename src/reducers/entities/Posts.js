cd755df10284705088323853571857e6c94cb0b3
issa-mour <38791976+issa-mour@users.noreply.github.com>
2018-08-02 09:04:30 +0300

    switch indention (#18)

--- src/reducers/entities/Posts.js
+++ src/reducers/entities/Posts.js
@@ -1,17 +1,17 @@
 import { fromJS } from 'immutable';
 import empty from 'is-empty';
 
-const INITIAL_STATE = fromJS({ byId : {} });
+const INITIAL_STATE = fromJS({ byId: {} });
 
 export default function (state = INITIAL_STATE, action) {
 	switch (action.type) {
-	default: {
-		const p = fromJS(action);
-		const posts = p.getIn(['payload', 'entities', 'posts'], {});
-		if (!empty(posts)) {
-			return state.set('byId', posts);
+		default: {
+			const p = fromJS(action);
+			const posts = p.getIn(['payload', 'entities', 'posts'], {});
+			if (!empty(posts)) {
+				return state.set('byId', posts);
+			}
+			return state;
 		}
-		return state;
-	}
 	}
 }
