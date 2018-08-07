2d46b5285cf4fb84a2df8733c420300c5508d90c
issa-mour <38791976+issa-mour@users.noreply.github.com>
2018-08-07 15:39:11 +0300

    Test] (#32)
    * messages has container
    
    * remove
    
    * tests

--- src/containers/comments/SocialCardCommentsContainer.test.js
+++ src/containers/comments/SocialCardCommentsContainer.test.js
@@ -1,47 +1,32 @@
-import { fromJS } from 'immutable';
 import React from 'react';
-import MockProvider, { getMockStore } from 'redux-mock-provider';
 import renderer from 'react-test-renderer';
 import { mount } from 'enzyme';
 
 import { SocialCardCommentsContainer } from './SocialCardCommentsContainer';
 
-const posts = fromJS({
-	'1': {
-		'id': 1,
-		'image': 'https://scontent.ftlv4-1.fna.fbcdn.net/v/t1.0-1/p80x80/19554009_10154562736535933_8492592550808364419_n.jpg?_nc_cat=0&oh=e2dcab39740d500b15ab15c33715c1cb&oe=5BEA19A5',
-		'profileText': 'Vevo',
-		'time': '3 hrs',
-		'text': 'Calling all Guns N\' Roses fans! Here\'s your chance to win the Appetite For Destruction Super Deluxe Box Set‼️ Enter now for your chance to win!',
-		'postImage': 'https://scontent.ftlv4-1.fna.fbcdn.net/v/t1.0-9/36907526_10155523573880933_4550184912837148672_n.jpg?_nc_cat=0&oh=907c9bbce64ffe2808a821fb673f140d&oe=5BA005E7',
-		'likes': 157,
-		'shares': 3,
-		'profileImage': 'https://scontent.ftlv4-1.fna.fbcdn.net/v/t1.0-1/p48x48/14572897_10209280775468762_3731006062202010506_n.jpg?_nc_cat=0&oh=41eba23bde8e0c43e709a6a9f9867c55&oe=5BDD6AD2',
-		'profileName': "Essa Mor",
-		"postComments": []
-	}
-});
-
-const store = getMockStore({
-	key: 'entities',
-	state: {}
-});
-
-store.getState = () => fromJS({});
-
-describe('SocialCardsContainer test', () => {
-	it.skip('renders correctly', () => {
+describe('SocialCardCommentsContainer test', () => {
+	it('renders correctly', () => {
 		const tree = renderer
-			.create(<MockProvider store={store}><SocialCardsContainer posts={posts}
-				fetchPosts={jest.fn()} likePost={jest.fn()} sharePost={jest.fn()} /></MockProvider>)
+			.create(<SocialCardCommentsContainer id={1} profileName="profileName" profileImage="profileImage" postComments={[]}/>)
 			.toJSON();
 		expect(tree).toMatchSnapshot();
 	});
 
-	it.skip('call fetchPosts', () => {
-		const callBack = jest.fn();
-		mount(<MockProvider store={store}><SocialCardsContainer posts={posts}
-			fetchPosts={callBack} likePost={jest.fn()} sharePost={jest.fn()} /></MockProvider>);
-		expect(callBack).toHaveBeenCalledTimes(1);
+	it('renders correctly - comment callback', () => {
+		const postId = 1,
+			profileName = "profileText",
+			profileImage = "profileImage",
+			comment = "hi";
+
+		const commentCallBack = jest.fn();
+		const wrapper = mount(<SocialCardCommentsContainer
+			id={postId}
+			commentOnPost={commentCallBack}
+			profileName={profileName}
+			profileImage={profileImage}
+			postComments={[]} />);
+		const input = wrapper.find('.Social_comments_input');
+		input.simulate('keyPress', { key: 'Enter', target: { value: 'hi' } });
+		expect(commentCallBack).toHaveBeenCalledWith({ postId, profileName, profileImage, comment });
 	});
 });
