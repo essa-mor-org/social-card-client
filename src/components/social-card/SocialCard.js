09a64f4dbaf5daf23daf3b29092e6460c228eaf5
issa-mour <issa.mour2@gmail.com>
2018-08-13 15:25:51 +0300

    reactions container

--- src/components/social-card/SocialCard.js
+++ src/components/social-card/SocialCard.js
@@ -1,40 +1,26 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import SocialCardHeader from './SocialCardHeader';
 import SocialCardPost from './SocialCardPost';
-import SocialCardReactionPanel from './reactions/SocialCardReactionPanel';
+import SocialCardReactionPanelContainer from '../../containers/reactions/SocialCardReactionPanelContainer';
 import SocialCardCommentsContainer from '../../containers/comments/SocialCardCommentsContainer';
 
 import './SocialCard.css';
 
-class SocialCard extends PureComponent {
-	render() {
-		const { id, image, profileText, time, text, postImage, like, likes, shares,
-			postComments, likeCallBack, shareCallBack } = this.props;
-		return (<div className="Social_card">
-			<SocialCardHeader
-				image={image}
-				profileText={profileText}
-				time={time} />
-			<SocialCardPost
-				text={text}
-				image={postImage}
-			/>
-			<SocialCardReactionPanel
-				likes={likes}
-				postComments={postComments}
-				shares={shares}
-				id={id}
-				like={like}
-				likes={likes}
-				shares={shares}
-				likeCallBack={likeCallBack}
-				shareCallBack={shareCallBack}
-			/>
-			<SocialCardCommentsContainer id={id} />
-		</div>);
-	}
+function SocialCard({ id, image, profileText, time, text, postImage }) {
+	return (<div className="Social_card">
+		<SocialCardHeader
+			image={image}
+			profileText={profileText}
+			time={time} />
+		<SocialCardPost
+			text={text}
+			image={postImage}
+		/>
+		<SocialCardReactionPanelContainer id={id} />
+		<SocialCardCommentsContainer id={id} />
+	</div>);
 }
 
 SocialCard.propTypes = {
@@ -43,13 +29,7 @@ SocialCard.propTypes = {
 	profileText: PropTypes.string.isRequired,
 	time: PropTypes.string.isRequired,
 	text: PropTypes.string.isRequired,
-	postImage: PropTypes.string.isRequired,
-	like: PropTypes.bool,
-	likes: PropTypes.number,
-	shares: PropTypes.number,
-	postComments: PropTypes.arrayOf(PropTypes.number).isRequired,
-	likeCallBack: PropTypes.func.isRequired,
-	shareCallBack: PropTypes.func.isRequired
+	postImage: PropTypes.string.isRequired
 };
 
 export default SocialCard;
